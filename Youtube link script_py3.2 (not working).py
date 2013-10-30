75c9a14cc7b7d591e3dd686a24469aa78cc05ad3
Tomislav Šulc <tomislav.sulc@foi.hr>
2013-10-30 12:10:24 +0100

    v1.0
    For python 2.7 and 3.2. Works fine with 2.7, have no time for 3.2 to
    check.
    
    Code improvements still needed.

--- Youtube link script_py3.2 (not working).py
+++ Youtube link script_py3.2 (not working).py
@@ -1,11 +1,11 @@
-import urllib2
+from urllib.request import urlopen
 import xml.etree.ElementTree as et
 import re
 import os
 
 more = 1
-id_playlist = raw_input("Enter youtube playlist id: ")
-number_of_iteration  = input("How much video links: ")
+id_playlist = input("Enter youtube playlist id: ")
+number_of_iteration  = int(input("How much video links: "))
 number = number_of_iteration / 50
 number2 = number_of_iteration % 50
 if (number2 != 0):
@@ -20,12 +20,12 @@ while more <= number3:
           start_index+=50
           
      str_start_index = str(start_index)
-     req = urllib2.Request('http://gdata.youtube.com/feeds/api/playlists/'+ id_playlist + '?v=2&&start-index=' + str_start_index + '&max-results=50')
-     response = urllib2.urlopen(req)
-     the_page = response.read()
+     req = urlopen('http://gdata.youtube.com/feeds/api/playlists/'+ id_playlist + '?v=2&&start-index=' + str_start_index + '&max-results=50')
+     #response = urllib2.urlopen(req)
+     the_page = req.read()
 
      #writing page in .xml
-     dat = open("web_content.xml","w")
+     dat = open("web_content.xml","wb")
      dat.write(the_page)
      dat.close()
 
@@ -50,7 +50,7 @@ while more <= number3:
      f = open ("links-"+ str_start_index +"to"+ str_till_links +".txt","r")
      link_all = f.read()
      new_string = link_all.replace("{'href': '","")
-     new_string2 = new_string.replace("', 'type': 'text/html', 'rel': 'alternate'}","")
+     new_string2 = new_string.replace("', 'rel': 'alternate', 'type': 'text/html'}","")
      f.close()
 
      #writing links to .txt
@@ -61,5 +61,4 @@ while more <= number3:
      more+=1
      
 os.remove('web_content.xml')
-print "Finished!"
-          
+print ("Finished!")
